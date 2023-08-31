db449c63549846e5222faeeb2bfd92db205876b1
tanisha <tanishamodi12@gmail.com>
2023-09-01 01:17:25 +0530

    steps page done

--- src/Components/Steps.js
+++ src/Components/Steps.js
@@ -1,6 +1,10 @@
 import React from "react";
 import steps from "../images/steps.png";
-import step1 from "../images/lastTry.png";
+import m1 from "../images/1.png";
+import m2 from "../images/2.png";
+import m3 from "../images/3.png";
+import m4 from "../images/4.png";
+import a1 from "../images/Vector 1.png";
 import "./Steps.css";
 
 function Steps() {
@@ -11,24 +15,32 @@ function Steps() {
         <div className="steps-left">
           <img src={steps} alt="" />
         </div>
-        <div className="steps1">
+        <div className="steps-right">
+          <p>Step 1 : Open setup and Click next button on dialog box.</p>
           <div className="s1">
-            <p>abs</p>
-            <img src={step1} alt="" />
+            <img className="step" src={m1} alt="" />
+            <div className="arr">
+              <img className="arrow" src={a1} alt="" />
+            </div>
           </div>
+          <p>Step 1 : Open setup and Click next button on dialog box.</p>
           <div className="s1">
-            <p>abs</p>
-            <img src={step1} alt="" />
+            <div className="arr">
+              <img className="flip arrow" src={a1} alt="" />
+            </div>
+            <img className="step" src={m2} alt="" />
           </div>
-        </div>
-        <div className="steps2">
-          <div className="s2">
-            <p>abs</p>
-            <img src={step1} alt="" />
+          <p>Step 1 : Open setup and Click next button on dialog box.</p>
+          <div className="s1">
+            <img className="step" src={m3} alt="" />
+            <div className="arr">
+              <img className="arrow" src={a1} alt="" />
+            </div>
           </div>
-          <div className="s2">
-            <p>abs</p>
-            <img src={step1} alt="" />
+          <p>Step 1 : Open setup and Click next button on dialog box.</p>
+          <div className="s1">
+            <div className="arr"></div>
+            <img className="step" src={m4} alt="" />
           </div>
         </div>
       </div>
